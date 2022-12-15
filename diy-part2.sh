28851c77fbdd4ab29466cc00296d4a0f82f9e04d
Richard Edinger <79996376+xiaozhile3689@users.noreply.github.com>
2022-12-15 16:10:41 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,4 +11,4 @@
 #
 
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/202.0.9.188/g' package/base-files/files/bin/config_generate
